af4c42a30b78ffc2035b8c4b2c908903970def5f
fattahjundi <fattahjundi97@gmail.com>
2023-05-01 15:23:00 +0700

    add path public

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 const port = process.env.PORT || 3000
 const monopoliRoute = require('./routes/monopoliRoute')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.set('view engine', 'ejs');
-app.use('/', express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(monopoliRoute)
 app.use(cors())
 
